b05ea45a1880b7bfb4c2a9dbbdc611cc4f9606d2
samarthwtf <samarthplayz1337@gmail.com>
2024-06-07 06:02:57 +0530

    middlewares

--- server2.js
+++ server2.js
@@ -3,29 +3,57 @@ import getNiggers from './niggaController.js';
 
 const PORT = process.env.PORT;
 
-const server = createServer((req, res) => {
-    if (req.url === '/api/niggers' && req.method === 'GET') {
-        res.setHeader('Content-type', 'application/json')
-        res.write(JSON.stringify(getNiggers()));
-        res.end();
-    }
-    else if (req.url.match(/\/api\/niggers\/([0-9]+)/) && req.method === 'GET') {
-        const id = req.url.split('/')[3];
-        const nigger = getNiggers().find((nigger) => nigger.id === parseInt(id));
-        res.setHeader('Content-type', 'application/json')
-        if (nigger) {
-            res.write(JSON.stringify(nigger));
-        } else {
-            res.statusCode = 404;
-            res.write(JSON.stringify({message: '404 ! Nigga not found'}));
-        }
-        res.end();
+// Logger Middleware
+
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next();
+}
+
+const jsonMiddleware = (req, res, next) => {
+    res.setHeader('Content-Type', 'application/json');
+    next();
+}
+
+const niggaHandler = (req, res) => {
+    res.write(JSON.stringify(getNiggers()));
+    res.end();
+}
+
+const NotFoundHandler = ( req , res ) => {
+    res.statusCode = 404;
+    res.write(JSON.stringify({message: '404 ! route not found'}));
+    res.end()
+}
+
+
+const getNiggerByID = (req, res) => {
+    const id = req.url.split('/')[3];
+    const nigger = getNiggers().find((nigger) => nigger.id === parseInt(id));
+    if (nigger) {
+        res.write(JSON.stringify(nigger));
     } else {
-        res.setHeader('Content-type', 'application/json')
         res.statusCode = 404;
-        res.write(JSON.stringify({message: '404 ! route not found'}));
-        res.end()
+        res.write(JSON.stringify({message: '404 ! Nigga not found'}));
     }
+    res.end();
+}
+
+const server = createServer((req, res) => {
+    logger(req, res, () => {
+        jsonMiddleware(req, res, () => {
+            if (req.url === '/api/niggers' && req.method === 'GET') {
+                niggaHandler(req, res)
+            }
+            else if (req.url.match(/\/api\/niggers\/([0-9]+)/) && req.method === 'GET') {
+                getNiggerByID(req, res)
+            } else {
+                NotFoundHandler(req, res)
+            }
+        })
+
+    });
+
 })
 
 server.listen(PORT, () => {
